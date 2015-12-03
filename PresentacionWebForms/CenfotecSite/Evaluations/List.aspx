55ea2f5720e134183f316e46503b4860828ccf7f
unknown <abermudezv@ucenfotec.ac.cr>
2015-12-03 11:06:28 -0600

    Make responsive the content of the page

---
+++ PresentacionWebForms/CenfotecSite/Evaluations/List.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="List.aspx.cs" Inherits="PresentacionWebForms.CenfotecSite.Evaluations.List" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <section class="content-header">
+        <h1>Evaluaciones
+              <small>Listar</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i>Inicio</a></li>
+            <li class="active">Evaluaciones</li>
+        </ol>
+    </section>
+    <br />
+    <section class="content">
+        <div class="table-responsive">
+            <asp:GridView ID="GridView1" CssClass="table table-hover" runat="server" >
+                <Columns>
+                    <asp:BoundField DataField="name" HeaderText="Nombre" />
+                    <asp:BoundField DataField="evaluado" HeaderText="Evaluado" />
+                    <asp:BoundField DataField="estado" HeaderText="Estado" />
+                    <asp:BoundField DataField="contador" HeaderText="Realizadas/Faltantes" />
+                    <asp:CommandField ShowEditButton="True" ShowSelectButton="True"  HeaderText="Acciones"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+
+
+    </section>
+</asp:Content>
---
+++ PresentacionWebForms/CenfotecSite/Evaluations/List.aspx.cs
@@ -0,0 +1,56 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PresentacionWebForms;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace PresentacionWebForms.CenfotecSite.Evaluations
+{
+    public partial class List : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadEvaluationsData();
+            }
+            
+            bindData();
+        }
+
+        private void bindData()
+        {
+            GridView1.DataSource = Session["EvaluationsTable"];
+            GridView1.DataBind();
+        }
+
+        private void loadEvaluationsData(){
+            //RestClient client = new RestClient(ConfigurationManager.AppSettings["endpoint"]);
+            //RestRequest request = new RestRequest("ducks", Method.GET);
+            //var response = client.Execute(request) as RestResponse;
+            //string json = response.Content;
+            //List<Duck> duckList = JsonConvert.DeserializeObject<List<Duck>>(json);
+
+            DataTable tableEvaluations = new DataTable("evaluations");
+            tableEvaluations.Columns.AddRange(new DataColumn[4]{
+                new DataColumn("name", typeof(string)),
+                new DataColumn("evaluado", typeof(string)),
+                new DataColumn("estado", typeof(string)),
+                new DataColumn("contador", typeof(string)),
+            });
+
+            tableEvaluations.Rows.Add("Eval 1", "Jose Romero", "Activa", "16/30");
+            tableEvaluations.Rows.Add("Eval 1", "Alvaro Cordero", "Activa", "16/30");
+            tableEvaluations.Rows.Add("Eval 1", "Kemly Cordoba", "Activa", "16/30");
+            tableEvaluations.Rows.Add("Eval 1", "Alvaro Ospina", "Activa", "16/30");
+
+            Session["EvaluationsTable"] = tableEvaluations;
+        }
+    }
+}
